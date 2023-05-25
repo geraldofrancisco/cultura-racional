47e5ddbf27c711b5c13501c2f4216bfd7fef28e3
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2023-05-25 20:11:58 -0300

    renomeado application

--- src/main/java/com/thor/culturaracional/CulturaRacionalApplication.java
+++ src/main/java/com/thor/culturaracional/CulturaRacionalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortariaApplication {
+public class CulturaRacionalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortariaApplication.class, args);
+		SpringApplication.run(CulturaRacionalApplication.class, args);
 	}
 
 }
